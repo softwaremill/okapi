2dce718254ffb9029b8af6826e7dd23947748ebd
Andrzej Kobyliński <endrju397@gmail.com>
2026-04-03 11:36:57 +0200

    build: create publish convention plugin with vanniktech + POM metadata

---
+++ buildSrc/src/main/kotlin/publish.gradle.kts
@@ -0,0 +1,34 @@
+package buildsrc.convention
+
+plugins {
+    id("com.vanniktech.maven.publish")
+}
+
+mavenPublishing {
+    publishToMavenCentral()
+    signAllPublications()
+
+    pom {
+        name.set(project.name)
+        description.set("Transactional outbox pattern for Kotlin/JVM")
+        url.set("https://github.com/softwaremill/okapi")
+        licenses {
+            license {
+                name.set("Apache-2.0")
+                url.set("https://www.apache.org/licenses/LICENSE-2.0")
+            }
+        }
+        developers {
+            developer {
+                id.set("softwaremill")
+                name.set("SoftwareMill")
+                url.set("https://softwaremill.com")
+            }
+        }
+        scm {
+            connection.set("scm:git:git://github.com/softwaremill/okapi.git")
+            developerConnection.set("scm:git:ssh://github.com/softwaremill/okapi.git")
+            url.set("https://github.com/softwaremill/okapi")
+        }
+    }
+}
